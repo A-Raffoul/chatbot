34ffdda5b22af1bb4c370170e9b3d058eba6b6ea
raffoul <anthony.raffoul11@gmail.com>
2024-08-19 19:48:00 +0200

    tesseract testing

--- pytessaract.py
+++ pytessaract.py
@@ -4,15 +4,19 @@ import pytesseract
 # Set the path to the Tesseract executable
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-print(pytesseract.get_tesseract_version())
-
-
-
 # Load an image using Pillow
-image = Image.open('samples/sample_image.png')
+# image_path='samples/sample_image.png'
+image_path='samples/flight-sc.png'
+# image_path = 'samples/PMC1626454_002_00.png'
+# image_path = 'samples/PMC3826085_003_00.png'
+# image_path = 'samples/image_1.png'
+image = Image.open(image_path)
 
 # Use pytesseract to perform OCR on the image
-text = pytesseract.image_to_string(image)
+text = pytesseract.image_to_data(image)
+print(text)
 
 # Print the extracted text
-print(text)
+output_file = 'pytessaract_output.txt'
+with open(output_file, 'w') as file:
+    file.write(text)
